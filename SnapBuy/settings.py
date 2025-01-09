457db636149151e7c819f15ae7c04ca95ea434dd
md mohsin <siam.mohsin2005@gmail.com>
2025-01-09 08:51:09 +0600

    update

--- SnapBuy/settings.py
+++ SnapBuy/settings.py
@@ -117,7 +117,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'product',)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
