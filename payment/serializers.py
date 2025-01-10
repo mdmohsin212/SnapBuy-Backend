a0fe406b04de1d50bcf3e49467ad7bfd8b59d855
md mohsin <siam.mohsin2005@gmail.com>
2025-01-10 20:41:14 +0600

    update

--- payment/serializers.py
+++ payment/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from product.serializers import *
 
 class CheckoutSerializers(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +8,15 @@ class CheckoutSerializers(serializers.ModelSerializer):
         fields = "__all__"
         
 class OrderItemSerializres(serializers.ModelSerializer):
+    product_img = serializers.SerializerMethodField()
+    product_title = serializers.SerializerMethodField()
     class Meta:
         model = OrderdItem
         fields = "__all__"
+        
+    def get_product_img(self, obj):
+        data = ProductSerializer(obj.product).data.get('img')
+        return f"https://snapbuy-backend.onrender.com{data}"
+    
+    def get_product_title(self, obj):
+        return ProductSerializer(obj.product).data.get('title')
