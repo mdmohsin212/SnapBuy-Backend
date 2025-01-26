5651b476b64369c8609ac0b59d2792ce7788b8c1
md mohsin <siam.mohsin2005@gmail.com>
2025-01-26 11:01:52 +0600

    update

--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Avg    
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -15,9 +16,18 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=1, max_digits=7)
     category = models.ManyToManyField(Category)
     img = models.ImageField(upload_to='images/')
-    
+
     def __str__(self):
         return self.name
+    
+    @property
+    def get_rating(self):
+        reviews = self.review_set.filter(product=self)
+        if reviews.exists():
+            total = sum([review.star for review in reviews])
+            rating = total / len(reviews)
+            return rating
+        return 0
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -28,11 +38,11 @@ class Cart(models.Model):
         return f"{self.user} - {self.product.name}"
 
 STAR_CHOICES = [
-    ('⭐', '⭐'),
-    ('⭐⭐', '⭐⭐'),
-    ('⭐⭐⭐', '⭐⭐⭐'),
-    ('⭐⭐⭐⭐', '⭐⭐⭐⭐'),
-    ('⭐⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'),
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
 ]
 
 class Review(models.Model):
@@ -40,7 +50,7 @@ class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    star = models.CharField(max_length=100, choices=STAR_CHOICES)
+    star = models.IntegerField(choices=STAR_CHOICES)
     body = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
     
