c1684377c7d51c9ead2eda01bea1810da2bb8676
md mohsin <siam.mohsin2005@gmail.com>
2025-02-08 20:16:51 +0600

    update

--- SnapBuy/urls.py
+++ SnapBuy/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import debug_toolbar
+# import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include("product.urls")),
     path('user/', include("account.urls")),
     path('payment/', include("payment.urls")),
-    path('__debug__/', include(debug_toolbar.urls)),
+    # path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
